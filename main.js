0560fe47bce526d23b3fb8398c965934f2bd5112
armygirlkrithika <92069801+armygirlkrithika@users.noreply.github.com>
2022-02-07 15:53:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,47 @@
-function setup()
-{
-video = createCapture(VIDEO);
-video.size(550, 500);
-canvas = createCanvas(500, 500);
-canvas.position(600, 100);
 
-poseNet = ml5.poseNet(video, modelLoaded);
-poseNet.on('pose', gotPoses);
+nose_X = 0;
+nose_Y = 0;
+difference = 0;
+
+leftWristX = 0;
+rightWristY = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550,600);
+    video.position(10,60);
+    canvas =  createCanvas(600,440);
+    canvas.position(600,130);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
-function modelLoaded()
-{
-    console.log('PoseNet Is Initialised');
-}
-function draw()
-{
-background('#00FF00');
-}
-function gotPoses(results)
-{
-    if(results.length > 0)
+function gotPoses(results) {
+    if (results.length > 0) 
     {
-        console.log(results);
-    }
+         console.log(results);
+    noseX=results[0].pose.nose.x;
+    noseY=results[0].pose.nose.y;
+    console.log("noseX="+noseX+"noseY="+noseY);
+    
+     leftWristX = results[0].pose.leftWrist.x;
+     rightWristY = results[0].pose.rightWrist.x;
+
+     difference = floor(leftWristX - rightWristY);
+     console.log(" left wrist = " + leftWristX + " right wrist = " + rightWristY + " difference =  " +  difference);
+    } 
+}
+
+function modelLoaded() {
+    console.log('poseNet is intialized');
+}
+
+
+function draw(){
+  background('#66b2ff');
+  document.getElementById("font_size").innerHTML="Width And Height of a text will be = " + difference +"px";
+  textSize(difference);
+  fill('#c200f7');
+  text('BTS',50,400);
+
 }
